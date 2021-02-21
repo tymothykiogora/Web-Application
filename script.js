cba95d7e81ccd42b1e87c26bd8b50a4115acc7e3
Your Name <you@example.com>
2021-02-21 13:23:18 +0300

    declare variables for male and female names

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var CC, YY, MM, DD, d, dayValue;
-var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
-
+// Declaring variables for female and male names.
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
