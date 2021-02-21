23667a5e0ce280de5e5ea0b57f988a32e1a5c3aa
Your Name <you@example.com>
2021-02-21 23:28:29 +0300

    add date input

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // Declaring variables for female and male names.
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+//create a function that will be called whenever you hit the submit button.
+function akanNames (){
+
+    //Declare date inputs and get its values
+ var DD=parseFloat(document.getElementById("dd").value);
+var MM=parseFloat(document.getElementById("mm").value);
+var YY=parseFloat(document.getElementById("yy").value);
+
+//calculate the century.
+var cc=(YY-1)/100+1;
+
+//form
