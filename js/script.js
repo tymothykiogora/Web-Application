b20c706a2c68d5c89ab789dd819f0e5567d6cd28
Your Name <you@example.com>
2021-03-10 14:09:53 +0300

    update js script link

---
+++ js/script.js
@@ -0,0 +1,78 @@
+var submission = function() {
+
+
+    var daysOfTheWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    var maleNames = [
+      "Kwasi",
+      "Kwadwo",
+      "Kwabena",
+      "Kwaku",
+      "Yaw",
+      "Kofi",
+      "Kwame"
+    ];
+    var femaleNames = [
+      "Akosua",
+       " Adwoa",
+      "Abenaa",
+      "Akua",
+      "Yaa",
+      "Afua",
+      "Ama"
+    ];
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+    var day = parseInt(document.getElementById("day").value);
+    
+    var date0fbirth = new Date(year + "/" + month + "/" + day);
+    var results = date0fbirth.getDay();
+    var output = document.getElementById("output");
+    var male = document.getElementById("m")
+    var female = document.getElementById("f")
+    
+    if (month =="" && year =="" && day =="" && name=="") {
+      alert("Please Enter you credentials");
+      return false;
+    }
+   
+   if (year < 0) {
+      output.style.background ="red"
+      output.style.color= "white"
+      output.innerHTML = "Hey! " + name + " please enter a valid year! "
+  }
+    
+  else if ((month < 1) || (month > 31)) {
+      output.style.background ="red"
+      output.style.color= "white"
+      output.innerHTML = "Hey! " + name + " please enter a valid month! "
+  }
+    
+   else if (day < 0 || day > 31) {
+      output.style.background ="red"
+      output.style.color= "white"
+      output.innerHTML = "Hey! " + name + " please enter a valid day! "
+  }
+  
+  
+    if(male.checked && year > 0 && month > 0 && month < 12 && day > 0 && day < 31) {
+      
+        console.log ("Hey! " + " you were born on a " + daysOfTheWeek[results]  +  "and your Akan name is " + maleNames[results]);
+    }
+     else if(female.checked && year > 0 && month > 0 && month < 12 && day > 0 && day < 32) {
+      
+      console.log  ("Hey! " + " you were born on a " + daysOfTheWeek[results] + " and your Akan name is " + femaleNames[results]);
+  }
+  
+    
+
+  }; 
+      
+ document.getElementById ("populate").addEventListener ("click",submission() )
